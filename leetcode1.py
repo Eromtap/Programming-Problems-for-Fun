dd9e11a553bb03f908a53e05da7627a01e0a7a8b
Eromtap <66063750+Eromtap@users.noreply.github.com>
2022-11-28 13:39:59 -0500

    Update leetcode1.py

--- leetcode1.py
+++ leetcode1.py
@@ -7,7 +7,7 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.
 
-Solved in Python cause I don't know Jave (yet).
+Solved in Python cause.
 
 '''
 
@@ -15,9 +15,6 @@ Solved in Python cause I don't know Jave (yet).
 
 
 
-
-nums = [2,7,11,15]
-target = 9
 
 class Solution(object):
     def twoSum(self, nums, target):
@@ -31,12 +28,16 @@ class Solution(object):
                 pos_count += 1
         if pos_count == len(nums) and target < 0:
             return []
-       
+
+            
         for i in nums:
             if i < 0:
                 neg_count += 1
         if neg_count == len(nums) and target >= 0:
             return []            
+
+
+
 
         for i in range(len(nums)-1):
             for j in range(i+1,len(nums)):
